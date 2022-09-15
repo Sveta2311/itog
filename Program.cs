35ff4ade62b5da42bc929e745986885f5660176c
Sveta <margaritka2132@mail.ru>
2022-09-15 19:34:07 +0300

    added GetArray and ReturnNewArray Functions

--- Program.cs
+++ Program.cs
@@ -9,3 +9,34 @@
         Console.WriteLine(errorMessage);
     }
 }
+string[] GetArray(int size)
+{
+    string[] myArray = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"\nВведите элемент массива под индексом {i}: ");
+        myArray[i] = Console.ReadLine();
+    }
+    return myArray;
+}
+
+static void ReturnNewArray(string[] myArray)
+{
+    string[] NewArray = new string[0];
+    foreach (var value in myArray)
+    {
+        if (value.Length <= 3)
+        {
+            Array.Resize(ref NewArray, NewArray.Length + 1);
+            NewArray[NewArray.Length - 1] = value;
+        }
+    }
+    if(NewArray.Length > 0)
+    {
+        Console.WriteLine(String.Join(", ", NewArray));
+    }
+    else
+    {
+        Console.WriteLine("Ни одного элемента массива <= 3 символов не найдено.");
+    }
+}
