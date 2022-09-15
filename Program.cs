4127081403dc19b6fe80df55f3cd76c1d686fa0d
Sveta <margaritka2132@mail.ru>
2022-09-15 19:32:47 +0300

    added GetArraySize Function

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int GetArraySize(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        int size;
+        if(int.TryParse(Console.ReadLine(), out size) && size > 0)
+            return size;
+        Console.WriteLine(errorMessage);
+    }
+}
